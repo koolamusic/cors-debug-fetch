87fa372917fee7f97240c9f5d468d86b1897331f
Andrew Miracle <me@andrewmiracle.com>
2021-05-08 14:19:20 +0000

    use browser reload

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const BASE_URL = "https://herokuapp.com/authenticator/login"
+(async () => {
+    const rawResponse = await fetch("https://api.herokuapp.com/login", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ a: 1, b: 'Textual content' })
+    });
+    const content = await rawResponse.json();
+    const root = document.getElementById("root")
+    root.innerHTML = JSON.stringify(content)
 
-    (async () => {
-        const rawResponse = await fetch(BASE_URL, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ a: 1, b: 'Textual content' })
-        });
-        const content = await rawResponse.json();
-        const root = document.getElementById("root")
-        root.innerHTML = JSON.stringify(content)
-
-        console.log(content);
-    })();
+    console.log(content);
+})();
